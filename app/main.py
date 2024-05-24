1f2908f21926a41aa99a2802ccb8301f189990f9
DeveloperJosh <bam0909@outlook.com>
2024-05-24 14:00:21 -0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -3,13 +3,13 @@ import sys
 
 def main():
     
-    valid_commands = {"exit", "echo"}
+    valid_commands = {"exit", "echo", "type"}
     
     while True:
         sys.stdout.write("$ ")
         sys.stdout.flush()
         user_input = input()
-        user_input_list = user_input.split(" ", 1)
+        user_input_list = user_input.lstrip().split()
         user_command = user_input_list[0] if len(user_input_list) > 0 else None
         user_args = user_input_list[1:] if len(user_input_list) > 1 else None
         if user_command is None:
@@ -19,13 +19,24 @@ def main():
             continue
         if user_command == "exit":
             break
-        if user_command == "echo":
+        elif user_command == "echo":
             if user_args is None:
                 print("")
                 continue
             else:
                 print(user_args[0])
                 continue
+        elif user_command == "type":
+            if user_args:
+                type_command = user_args[0]
+                if type_command in valid_commands:
+                    print(f"{type_command}: is a shell builtin")
+                    continue
+                else:
+                    print(f"{type_command}: not found")
+                    continue
+        else:
+            continue
         break
     return 0
 
