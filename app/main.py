7ac118f34f2a77c26f8697701186f165ff436cef
DeveloperJosh <bam0909@outlook.com>
2024-05-24 13:53:39 -0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -3,15 +3,24 @@ import sys
 
 def main():
     
-    valid_commands = []
+    valid_commands = {"exit"}
     
     while True:
         sys.stdout.write("$ ")
         sys.stdout.flush()
-        user_command = input()
-        if user_command not in valid_commands:
-            print(f"${user_command}: command not found")
+        user_input = input()
+        user_input_list = user_input.split(" ", 1)
+        user_command = user_input_list[0] if len(user_input_list) > 0 else None
+        user_args = user_input_list[1:] if len(user_input_list) > 1 else None
+        if user_command is None:
             continue
+        if user_command not in valid_commands:
+            print(f"{user_command}: command not found")
+            continue
+        if user_command == "exit":
+            break
+        break
+    return 0
 
 
 if __name__ == "__main__":
