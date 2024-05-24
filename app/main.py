3f829b91cb5edb39429d1aaa484aed4c31ea8bcf
DeveloperJosh <bam0909@outlook.com>
2024-05-24 13:32:35 -0400

    pass 1st stage

--- app/main.py
+++ app/main.py
@@ -2,10 +2,15 @@ import sys
 
 
 def main():
+    
+    vaild_commands = {}
+    
     while True:
         sys.stdout.write("$ ")
         sys.stdout.flush()
-        input()
+        user_commands = input()
+        if user_commands not in vaild_commands:
+            print(f"Command {user_commands} not found")
 
 
 if __name__ == "__main__":
