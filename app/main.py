b9eb8555ea21084f262fc6efbd8cce481c7acd19
DeveloperJosh <bam0909@outlook.com>
2024-05-24 14:41:27 -0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,55 +2,61 @@ import os
 import sys
 from typing import Optional
 
-
 def main():
-    
     PATH = os.environ.get("PATH", None)
-    
-    valid_commands = {"exit", "echo", "type"}
-    
+    shell_builtins = {"exit", "echo", "type"}
+
     def path_value(cmd: str, path: str) -> Optional[str]:
         potential_paths = path.split(":")
         for potential_path in potential_paths:
-            full_path: str = potential_path + "/" + cmd
+            full_path: str = os.path.join(potential_path, cmd)
             if os.path.exists(full_path):
                 return full_path
         return None
+
     def get_type(cmd: str) -> None:
         found_path = path_value(cmd, PATH) if PATH else None
-        if cmd in valid_commands:
+        if cmd in shell_builtins:
             print(f"{cmd} is a shell builtin")
         elif found_path:
             print(f"{cmd} is {found_path}")
         else:
             print(f"{cmd}: not found")
+            
+    def is_executable(cmd: str) -> bool:
+        if os.path.exists(cmd):
+            return True
+        if PATH and path_value(cmd, PATH):
+            return True
+        return False
 
-    
     while True:
-        sys.stdout.write("$ ")
-        sys.stdout.flush()
-        user_input = input()
-        user_input_list = user_input.lstrip().split()
-        user_command = user_input_list[0] if len(user_input_list) > 0 else None
-        user_args = user_input_list[1:] if len(user_input_list) > 1 else None
-        if user_command is None:
+        user_input = input("$ ").strip().split()
+        if not user_input:
             continue
-        if user_command not in valid_commands:
-            print(f"{user_command}: command not found")
-            continue
-        if user_command == "exit":
-            break
-        elif user_command == "echo":
-            print(*user_args) if user_args else print("")
-            continue
-        elif user_command == "type":
-            if user_args:
+        user_command, *user_args = user_input
+
+        if user_command in shell_builtins:
+            if user_command == "exit":
+                break
+            elif user_command == "echo":
+                print(*user_args)
+            elif user_command == "type":
                 for cmd in user_args:
                     get_type(cmd)
+        elif is_executable(user_command):
+            cmd = (
+                user_command + " " + " ".join(a for a in user_args)
+                if user_args
+                else user_command
+            )
+            os.system(cmd)
         else:
-            continue
-    return 0
-
+            found_path = path_value(user_command, PATH)
+            if found_path:
+                os.execv(found_path, [user_command] + user_args)
+            else:
+                print(f"{user_command}: not found")
 
 if __name__ == "__main__":
     main()
