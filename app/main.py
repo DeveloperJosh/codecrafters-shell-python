350007c960f66118b45ffe73e6b30021b7228070
DeveloperJosh <bam0909@outlook.com>
2024-05-24 13:40:46 -0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -3,14 +3,14 @@ import sys
 
 def main():
     
-    vaild_commands = {}
+    valid_commands = []
     
     while True:
         sys.stdout.write("$ ")
         sys.stdout.flush()
-        user_commands = input()
-        if user_commands not in vaild_commands:
-            print(f"Command {user_commands} not found")
+        user_command = input()
+        if user_command not in valid_commands:
+            print(f"${user_command}: command not found")
 
 
 if __name__ == "__main__":
