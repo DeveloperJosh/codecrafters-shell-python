8328ff806f4102acca3f0a5ebd643520abb22549
DeveloperJosh <bam0909@outlook.com>
2024-05-24 13:42:52 -0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -11,6 +11,7 @@ def main():
         user_command = input()
         if user_command not in valid_commands:
             print(f"${user_command}: command not found")
+            continue
 
 
 if __name__ == "__main__":
